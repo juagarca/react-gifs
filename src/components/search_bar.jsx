51b69c0e94005b5ad998b7479d0907387749ccb6
Juan Garcia <juagarca@gmail.com>
2020-12-17 12:26:26 +0000

    search bar component created

---
+++ src/components/search_bar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// console.log("hello from search bar");
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <input type="text" className="form-search form-control" />
+    )
+  }
+}
+
+export default SearchBar;
