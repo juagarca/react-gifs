7b72f07f49035e47943c65626f73b196621c02fe
Juan Garcia <juagarca@gmail.com>
2020-12-17 13:33:18 +0000

    giflist component created

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar';
 import Gif from './gif';
+import GifList from './gif_list';
+
 
 class App extends Component {
   render() {
+    const gifs = [
+      { id: 'wUdz3pB1wAMhNgPLzn' },
+      { id: 'wUdz3pB1wAMhNgPLzn' },
+      { id: 'wUdz3pB1wAMhNgPLzn' },
+      { id: 'wUdz3pB1wAMhNgPLzn' },
+      { id: 'wUdz3pB1wAMhNgPLzn' },
+      { id: 'wUdz3pB1wAMhNgPLzn' },
+      { id: 'wUdz3pB1wAMhNgPLzn' },
+      { id: 'wUdz3pB1wAMhNgPLzn' },
+      { id: 'wUdz3pB1wAMhNgPLzn' },
+      { id: 'wUdz3pB1wAMhNgPLzn' },
+      { id: 'wUdz3pB1wAMhNgPLzn' }
+    ];
+
     return (
       <div>
         <div className="left-scene">
           <SearchBar />
           <div className="selected-gif">
-            <Gif id='wUdz3pB1wAMhNgPLzn' />
+            <Gif id="wUdz3pB1wAMhNgPLzn" />
           </div>
         </div>
         <div className="right-scene">
+          <GifList gifs={gifs} />
         </div>
       </div>
     );
